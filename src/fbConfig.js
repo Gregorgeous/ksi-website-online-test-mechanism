a47e32ce78427cfa7a7948318a105e93571aac52
Gregorgeous <g.r.fisher.pl@gmail.com>
2018-01-21 20:14:23 +0000

    Delete fbConfig.js

--- src/fbConfig.js
+++
@@ -1,8 +0,0 @@
-export default {
-  apiKey: "AIzaSyBFmzyLdcH3NLUSTagqkmtCf63tKkxCPCU",
-  authDomain: "ksi-test-mechanism.firebaseapp.com",
-  databaseURL: "https://ksi-test-mechanism.firebaseio.com",
-  projectId: "ksi-test-mechanism",
-  storageBucket: "ksi-test-mechanism.appspot.com",
-  messagingSenderId: "1066220391714"
-}
